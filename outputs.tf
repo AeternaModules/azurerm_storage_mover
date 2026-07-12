2a5a855d3c94ea1576865d31eec0e917b59895ea
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:48 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "storage_movers" {
-  description = "All storage_mover resources"
-  value       = azurerm_storage_mover.storage_movers
-}
 output "storage_movers_description" {
-  description = "List of description values across all storage_movers"
-  value       = [for k, v in azurerm_storage_mover.storage_movers : v.description]
+  description = "Map of description values across all storage_movers, keyed the same as var.storage_movers"
+  value       = { for k, v in azurerm_storage_mover.storage_movers : k => v.description }
 }
 output "storage_movers_location" {
-  description = "List of location values across all storage_movers"
-  value       = [for k, v in azurerm_storage_mover.storage_movers : v.location]
+  description = "Map of location values across all storage_movers, keyed the same as var.storage_movers"
+  value       = { for k, v in azurerm_storage_mover.storage_movers : k => v.location }
 }
 output "storage_movers_name" {
-  description = "List of name values across all storage_movers"
-  value       = [for k, v in azurerm_storage_mover.storage_movers : v.name]
+  description = "Map of name values across all storage_movers, keyed the same as var.storage_movers"
+  value       = { for k, v in azurerm_storage_mover.storage_movers : k => v.name }
 }
 output "storage_movers_resource_group_name" {
-  description = "List of resource_group_name values across all storage_movers"
-  value       = [for k, v in azurerm_storage_mover.storage_movers : v.resource_group_name]
+  description = "Map of resource_group_name values across all storage_movers, keyed the same as var.storage_movers"
+  value       = { for k, v in azurerm_storage_mover.storage_movers : k => v.resource_group_name }
 }
 output "storage_movers_tags" {
-  description = "List of tags values across all storage_movers"
-  value       = [for k, v in azurerm_storage_mover.storage_movers : v.tags]
+  description = "Map of tags values across all storage_movers, keyed the same as var.storage_movers"
+  value       = { for k, v in azurerm_storage_mover.storage_movers : k => v.tags }
 }
 
